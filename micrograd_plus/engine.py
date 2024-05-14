554a38dc4ca9f93b6d60dd32cdb2ead067e399e2
Siddarth <siddarth_ch@outlook.com>
2024-05-14 13:38:16 +0530

    added engine.py and some tests

---
+++ micrograd_plus/engine.py
@@ -0,0 +1,118 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+from graphviz import Digraph
+
+class Value:
+  #represents a value object - a scalar value and it's gradient
+
+  def __init__(self, data, _children=(), _op="", label = ""):
+    self.data = data
+    self._prev = set(_children)
+    self._op = _op
+    self.label = label
+    self.grad = 0.0
+    self._backward = lambda: None
+    self._draw = lambda:None
+  
+  def tanh(self):
+    x = self.data
+    t = (math.exp(2*x)-1)/(math.exp(2*x)+1)
+    out = Value(t, (self,), "tanh")
+
+    def _backward():
+      self.grad += (1 - t ** 2) ** out.grad
+    out._backward = _backward
+    
+    return out 
+  
+  def exp(self):
+    x = self.data
+    e = math.exp(x)
+    out = Value(e, (self,), "exp")
+
+    def _backward():
+      self.grad = e * out.grad
+    out._backward = _backward
+
+    return out
+  
+  def backward(self):
+    # topological sort
+    topo = []
+    visited = set()
+    def build_topo(v):
+      if v not in visited:
+        visited.add(v)
+        for child in v._prev:
+          build_topo(child)
+        topo.append(v)
+    build_topo(self)
+
+    self.grad = 1
+    for node in reversed(topo):
+      node._backward()
+
+  def draw(self):
+    def trace(root):
+      # builds a set of all nodes and edges in a graph
+      nodes, edges = set(), set()
+      def build(v):
+        if v not in nodes:
+          nodes.add(v)
+          for child in v._prev:
+            edges.add((child, v))
+            build(child)
+      build(root)
+      return nodes, edges
+    
+    dot = Digraph(format='svg', graph_attr={'rankdir': 'LR'}) # LR = left to right
+
+    nodes, edges = trace(self)
+    for n in nodes:
+      uid = str(id(n))
+      # for any value in the graph, create a rectangular ('record') node for it
+      dot.node(name = uid, label = "{ %s | data %.4f | grad %.4f }" % (n.label, n.data, n.grad), shape='record')
+      if n._op:
+        # if this value is a result of some operation, create an op node for it
+        dot.node(name = uid + n._op, label = n._op)
+        # and connect this node to it
+        dot.edge(uid + n._op, uid)
+
+    for n1, n2 in edges:
+      # connect n1 to the op node of n2
+      dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+
+    return dot
+  
+  def __repr__(self):
+    return f"Value(data={self.data})"
+  
+  def __add__(self, other):
+    other = other if isinstance(other, Value) else Value(other)
+    out = Value(self.data + other.data, (self, other), "+")
+
+    def _backward():
+      self.grad += 1.0 * out.grad # previously = 1.0 * out.grad which caused backpropogation bug when same node used twice
+      other.grad += 1.0 * out.grad
+    out._backward = _backward
+
+    return out
+  
+  def __radd__(self, other):
+    return self + other
+
+  def __mul__(self, other):
+    other = other if isinstance(other, Value) else Value(other)
+    out = Value(self.data * other.data, (self, other), "*")
+
+    def _backward():
+      self.grad += other.data * out.grad 
+      other.grad += self.data * out.grad
+    out._backward = _backward
+
+    return out
+  
+  def __rmul__(self, other):
+    return self * other
+
---
+++ test_engine.py
@@ -0,0 +1,13 @@
+from micrograd_plus.engine import Value
+
+
+a = Value(2.0, label='a')
+b = Value(-3.0, label='b')
+c = Value(10.0, label='c')
+e = a*b; e.label = 'e'
+d = e + c; d.label = 'd'
+f = Value(-2.0, label='f')
+L = d * f; L.label = 'L'
+print(L)
+L.backward()
+L.draw().view()
