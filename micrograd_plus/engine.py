38aeaa072b13c715909b10ccf9de8a0cab340c45
Siddarth <siddarth_ch@outlook.com>
2024-05-15 10:01:01 +0530

    fixed bug in Value and added NN

--- micrograd_plus/engine.py
+++ micrograd_plus/engine.py
@@ -13,7 +13,6 @@ class Value:
     self.label = label
     self.grad = 0.0
     self._backward = lambda: None
-    self._draw = lambda:None
   
   def tanh(self):
     x = self.data
@@ -21,7 +20,7 @@ class Value:
     out = Value(t, (self,), "tanh")
 
     def _backward():
-      self.grad += (1 - t ** 2) ** out.grad
+      self.grad += (1 - t ** 2) * out.grad
     out._backward = _backward
     
     return out 
@@ -85,9 +84,6 @@ class Value:
 
     return dot
   
-  def __repr__(self):
-    return f"Value(data={self.data})"
-  
   def __add__(self, other):
     other = other if isinstance(other, Value) else Value(other)
     out = Value(self.data + other.data, (self, other), "+")
@@ -115,4 +111,25 @@ class Value:
   
   def __rmul__(self, other):
     return self * other
+  
+  def __neg__(self):
+    return self * -1
+  
+  def __sub__(self, other):
+    return self + (-other)
+  
+  def __truediv__(self, other):
+    return self * other**-1
+  
+  def __pow__(self, other):
+    out = Value(self.data**other, (self, ), f"**{other}")
+
+    def _backward():
+      self.grad += (other * (self.data ** (other - 1))) * out.grad
+    out._backward = _backward
+
+    return out
+  
+  def __repr__(self):
+    return f"Value(data={self.data})"
 
--- test_engine.py
+++ test_engine.py
@@ -8,6 +8,7 @@ e = a*b; e.label = 'e'
 d = e + c; d.label = 'd'
 f = Value(-2.0, label='f')
 L = d * f; L.label = 'L'
+L = L**2
 print(L)
 L.backward()
 L.draw().view()
